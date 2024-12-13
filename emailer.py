3aeb5771c304ecacb0cca077f4d36c537cd74dd2
Aidanjaffa <leeaidanjames@gmail.com>
2024-12-13 16:29:27 +0000

    multi threading and commands to input new data

--- emailer.py
+++ emailer.py
@@ -1,31 +1,63 @@
-import pandas
 import smtplib
 import datetime, time
 from email.mime.text import MIMEText
+import threading
 
-doc = pandas.read_excel("jobs.xlsx")
-rows = doc.shape[0]
+send_list = [["x@gmail.com", "2099-12-13 16:27:00", "Aidan"]]
 
-send_list = [["xx11shadowman11Xx@gmail.com", "2024-12-13 15:22:00", "Aidan"]]
+message = "Hi My name is aidan, i made an application with you, i just wanted to follow up on my application and see how everything is going"
 
 sender = "xX11shadowman11Xx@gmail.com"
 password = "ydaj hvpl bjnw vmtc"
 
-with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-    print("Logged In")
-    server.login(sender, password)
+lock = threading.Lock()
 
-    while len(send_list) != 0:
-        for data in send_list:
-            msg = MIMEText(str(data))
-            date = data[1]
-            current = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            print(f"waiting to send to {data[0]} at time {date}. Current Time: {current} ")
+def new_data():
+    date = input("Input sending date: ")
+    email = input("Input Recipient Email: ")
+    name = input("Input company name: ")
 
-            if str(date) < current:
-                server.sendmail(sender, data[0], msg.as_string())
-                send_list.remove(data)
-                print(f"Sent to {data[0]}")
-        time.sleep(1)
+    with lock:
+        send_list.append([email, date, name])
+        print ("New Data Added")
+
+def email():
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        print("Logged In")
+        server.login(sender, password)
+
+        while len(send_list) != 0:
+            with lock:
+                for data in send_list:
+                    msg = MIMEText(message)
+                    msg["subject"] = "Application follow up"
+                    date = data[1]
+                    current = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    #print(f"waiting to send to {data[0]} at time {date}. Current Time: {current} ")
+
+                    if str(date) < current:
+                        server.sendmail(sender, data[0], msg.as_string())
+                        send_list.remove(data)
+                        print(f"\nSent to {data[0]}")
+            time.sleep(1)
 
 
+email_thread = threading.Thread(target=email)
+email_thread.start()
+time.sleep(0.5)
+while True:
+    command = input("=> ")
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    match command:
+        case "new":
+            new_data()
+        case "status":
+            for item in send_list:
+                print(f"Pending Delivery To {item[0]}, Delivery Time: {item[1]}, Current Time: {now}")
+        case "time":
+            print(now)
+        case "delete":
+            for item in send_list:
+                print (item)
+            deleted = int(input("delete: "))
+            del send_list[deleted]
