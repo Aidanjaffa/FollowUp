bc91552351dfdcfd78ed6be75b09c2fef4c3b818
Aidanjaffa <leeaidanjames@gmail.com>
2024-12-13 15:23:18 +0000

    email timer update

--- emailer.py
+++ emailer.py
@@ -1,33 +1,31 @@
 import pandas
 import smtplib
-import time
+import datetime, time
 from email.mime.text import MIMEText
 
 doc = pandas.read_excel("jobs.xlsx")
 rows = doc.shape[0]
 
-send_list = []
+send_list = [["xx11shadowman11Xx@gmail.com", "2024-12-13 15:22:00", "Aidan"]]
 
 sender = "xX11shadowman11Xx@gmail.com"
 password = "ydaj hvpl bjnw vmtc"
-
-for i, row in doc.iterrows():
-    email = row["Email"]
-    date = row["Date"]
-    company = row["Company"]
-
-    send_list.append([email, date, company])
-
 
 with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
     print("Logged In")
     server.login(sender, password)
 
-    for data in send_list:
-        msg = MIMEText(str(data))
+    while len(send_list) != 0:
+        for data in send_list:
+            msg = MIMEText(str(data))
+            date = data[1]
+            current = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print(f"waiting to send to {data[0]} at time {date}. Current Time: {current} ")
 
-        server.sendmail(sender, data[0], msg.as_string())
-        print(f"Sent to {data[0]}")
-
+            if str(date) < current:
+                server.sendmail(sender, data[0], msg.as_string())
+                send_list.remove(data)
+                print(f"Sent to {data[0]}")
+        time.sleep(1)
 
 
